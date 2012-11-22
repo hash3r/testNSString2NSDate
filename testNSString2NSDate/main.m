4777b0f041eae97722cdf54b3ef754b347b156c6
hash3r <vovagn@ya.ru>
2012-11-23 00:05:03 +0200

    add  gitignore

--- testNSString2NSDate/main.m
+++ testNSString2NSDate/main.m
@@ -11,12 +11,13 @@
 int main(int argc, const char * argv[])
 {
 
-	@autoreleasepool {
-	    
-	    // insert code here...
-	    NSLog(@"Hello, World!");
-	    
-	}
+	NSString * date = @"201109"; // исходная строка
+	NSDateFormatter * date_formatter = [[[NSDateFormatter alloc] init] autorelease];
+	[date_formatter setDateFormat: @"yyyyMM"]; // описали формат ISO-8601
+	NSDate * result = [date_formatter dateFromString: date]; // конвертирование
+	NSLog (@"%@", [result description]); // так можно выводить NSDate в лог
+	
+
     return 0;
 }
 
