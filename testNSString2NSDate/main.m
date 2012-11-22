d8575d097e4a3ba8ce5229241cc7dda630b9239a
hash3r <vovagn@ya.ru>
2012-11-22 23:51:31 +0200

    init commit

---
+++ testNSString2NSDate/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  testNSString2NSDate
+//
+//  Created by Andrew on 22.11.12.
+//  Copyright (c) 2012 Andrew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
